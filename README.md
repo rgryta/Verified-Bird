30a132eff70963641c6b52f803a280e54f24ca76
Radosław Gryta <radek.gryta@gmail.com>
2022-11-07 19:21:56 +0100

    Extension Overhaul
    Create PRIVACY_POLICY, README, LICENSE

---
+++ README.md
@@ -0,0 +1,31 @@
+# Verified-Bird
+<div align="center">
+  <p>
+    <img src="https://raw.githubusercontent.com/rgryta/Verified-Bird/main/icons/icon128.png" width="128" height="128" alt="Verified Bird" />
+  </p>
+</div>
+
+
+## About
+
+This is official repository of Verified Bird - get rid of unnecessary "Verified" checkmark from Twitter in one click. If you wish to use a different emoji or text, so be it. Simply open extension page (click on extension icon) and customize it.
+
+## How to install
+
+1. Download source code .zip file - from Releases or [directly from Github page](https://github.com/rgryta/Verified-Bird/archive/refs/heads/main.zip).
+1. Unpack the package file to a separate directory.
+1. Head over to  [chrome://extensions](chrome://extensions) within Google Chrome (or corresponding packe in a different browser).
+1. Enable "Developer Mode" to the top right.
+1. Click on "Load Unpacked" from the developer toolbar and choose the unpacked directory from point (2).
+
+## How to use
+
+1. By default the checkmark will be replaced with a clown emoji.
+1. Extension (should) work everywhere - not counting screenshots of course. Twitter's page or another one with an embedded tweet - checkmark is gone.
+1. You can customize the replacement (or quickly disable the extension) by clicking on its icon and saving new settings.
+
+## Disclaimer
+
+This is a parody project meant to showcase how unnecessary the verification mark has become after allowing everybody to get their own by subscribing to Twitter Blue. Who knows how many verified Elon Musks there's roaming on the Dark Twitter Planes these days.
+
+Official download link straight from the Chrome Web Store coming soon.
--- settings.js
+++ settings.js
@@ -1,39 +1,183 @@
-// Saves options to chrome.storage
-function save_options() {
-	
-  var toggle = document.getElementById('toggle').checked;
-  var replacer = document.getElementById('replacer').value;
-  
-  chrome.storage.sync.set({
-    toggle: toggle,
-    replacer: replacer
-  }, function() {
-	  
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-	
-  });
+// Replacer text boxes
+function show_replacer(el) {
+    el.classList.add('enabled');
 }
 
-function restore_options() {
-  chrome.storage.sync.get({
-    toggle: true,
-    replacer: "🤡"
-  }, function(items) {
-    document.getElementById('toggle').checked = items.toggle;
-    document.getElementById('replacer').value = items.replacer;
-  });
-  
-  document.getElementById('reset').addEventListener('click', function(){
-	  document.getElementById('toggle').checked = true;
-	  document.getElementById('replacer').value = '🤡';
-	  save_options();
-  });
-  document.getElementById('save').addEventListener('click', save_options);
+function hide_replacer(el) {
+    el.classList.remove('enabled');
 }
 
+// Initialize logic for verified users section
+function init_v() {
+    let dis = document.querySelector('#v-norm');
+    let en = document.querySelector('#v-repl');
 
-document.addEventListener('DOMContentLoaded', restore_options);
+    let textField = document.querySelector('#v-repl-val');
+
+
+    dis.addEventListener('change', function() {
+        hide_replacer(textField);
+
+		// Logic to check if extension is enabled for verified or non-verified users
+        let veri_dis = true;
+        let nveri_dis = document.querySelector('#nv-norm').checked;
+
+		
+        chrome.storage.sync.set({
+            toggle: !(veri_dis && nveri_dis),
+            repVer: !veri_dis
+        }, null);
+    });
+    en.addEventListener('change', function() {
+        show_replacer(textField);
+
+        chrome.storage.sync.set({
+            toggle: true,
+            repVer: true
+        }, null);
+    });
+}
+
+// Initialize logic for non-verified users section
+function init_nv() {
+    let dis = document.querySelectorAll('#nv-norm,#nv-veri');
+    let en = document.querySelector('#nv-repl');
+
+    let textField = document.querySelector('#nv-repl-val');
+
+    dis.forEach(function(i) {
+        i.addEventListener('change', function() {
+            hide_replacer(textField);
+
+			// Logic to check if extension is enabled for verified or non-verified users
+            let veri_dis = document.querySelector('#v-norm').checked;
+            let nveri_dis = i.id == 'nv-norm';
+			
+			
+            chrome.storage.sync.set({
+                toggle: !(veri_dis && nveri_dis),
+                repNonver: !nveri_dis,
+                nonverCustom: false
+            }, null);
+        });
+    });
+
+    en.addEventListener('change', function() {
+        show_replacer(textField);
+
+        chrome.storage.sync.set({
+            toggle: true,
+            repNonver: true,
+            nonverCustom: true
+        }, null);
+    });
+}
+
+// Initialize logic for power button - off and on - plus listener for its calculatable status
+function extension_status() {
+    let ext_status = document.querySelector('#ext-status');
+
+    chrome.storage.sync.get({
+        toggle: true
+    }, function(items) {
+        if (items.toggle) {
+            ext_status.classList.add('ext-enabled');
+        } else {
+            ext_status.classList.remove('ext-enabled');
+        }
+    });
+
+    chrome.storage.onChanged.addListener(function(changes, namespace) {
+        for (let [key, {
+                oldValue,
+                newValue
+            }] of Object.entries(changes)) {
+            if (key == 'toggle') {
+                let ext_status = document.querySelector('#ext-status');
+                if (newValue) {
+                    ext_status.classList.add('ext-enabled');
+                } else {
+                    ext_status.classList.remove('ext-enabled');
+                }
+            }
+        }
+    });
+
+    ext_status.addEventListener('click', function() {
+        let toggle = ext_status.classList.contains('ext-enabled');
+        if (toggle) {
+            document.querySelector('#v-norm').click();
+            document.querySelector('#nv-norm').click();
+        } else {
+            document.querySelector('#v-repl').click();
+            document.querySelector('#nv-veri').click();
+        }
+    });
+}
+
+// Reset replacement strings with default values
+function reset_settings() {
+    document.querySelector('#ext-reset').addEventListener('click', function() {
+        chrome.storage.sync.set({
+            verReplacer: '🤡',
+            nonverReplacer: '😎'
+        }, null);
+        document.querySelector('#v-repl-val').value = '🤡';
+        document.querySelector('#nv-repl-val').value = '😎';
+    });
+}
+
+// Set up custom replacement listeners
+function replacers() {
+    document.querySelector('#v-repl-val').addEventListener('input', function(e) {
+        console.log(e.target.value);
+        chrome.storage.sync.set({
+            verReplacer: e.target.value
+        }, null);
+    });
+    document.querySelector('#nv-repl-val').addEventListener('input', function(e) {
+        console.log(e.target.value);
+        chrome.storage.sync.set({
+            nonverReplacer: e.target.value
+        }, null);
+    });
+}
+
+// Set up extension page content
+function init() {
+	// Recover settings from sync
+    chrome.storage.sync.get({
+        repVer: true,
+        repNonver: true,
+        nonverCustom: false,
+
+        verReplacer: '🤡',
+        nonverReplacer: '😎'
+    }, function(items) {
+
+        if (items.repVer) {
+            document.querySelector('#v-repl').click()
+        } else {
+            document.querySelector('#v-norm').click()
+        }
+
+        if (items.nonverCustom) {
+            document.querySelector('#nv-repl').click()
+        } else if (items.repNonver) {
+            document.querySelector('#nv-veri').click()
+        } else {
+            document.querySelector('#nv-norm').click()
+        }
+
+        document.querySelector('#v-repl-val').value = items.verReplacer;
+        document.querySelector('#nv-repl-val').value = items.nonverReplacer;
+    });
+
+    init_v();
+    init_nv();
+    extension_status();
+    reset_settings();
+    replacers();
+}
+
+document.addEventListener('DOMContentLoaded', init);
